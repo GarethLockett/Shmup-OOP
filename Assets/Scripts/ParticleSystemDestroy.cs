e86f37fd539323b21b603b69196267ab6e22e966
Gareth Lockett <g.lockett@sae.edu>
2020-03-15 10:41:21 +0800

    ParticleSystem Destroy
    Added ParticleSystemDestroy script to clean up particle systems after they have finished.

---
+++ Assets/Scripts/ParticleSystemDestroy.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+    Script: ParticleSystemDestroy
+    Author Gareth lockett
+    Version: 1.0
+    Description:    Destroys the game object a particle system is attached to when it is no longer emitting particles.
+*/
+
+[ RequireComponent( typeof( ParticleSystem ) ) ]
+public class ParticleSystemDestroy : MonoBehaviour
+{
+    // Properties
+    private ParticleSystem pSystem;
+
+    // Methods
+    private void Start()
+    {
+        this.pSystem = this.gameObject.GetComponent<ParticleSystem>();
+    }
+
+    private void Update()
+    {
+        if( this.pSystem.IsAlive() == false ) { Destroy( this.gameObject ); }
+    }
+}
